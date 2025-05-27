96476709a2c966f2f1dc6f6318cbee9775d98e1e
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-27 10:24:45 +0530

    ignore added

--- test_bulk_download.py
+++ test_bulk_download.py
@@ -1,11 +1,6 @@
 from downloader import bulk_download_reels
 
-shortcodes = [
-    "C5yW9A3PqJ9",
-    "C5xXQHwpq1Z",
-    "C5zC3pQjF4R",
-    # Add your shortcodes here manually
-]
+shortcodes = ['DKEjzAShOTL', 'DJ_RHI3hri7', 'DJ66U3ZBk8r', 'DJ086tyh5Yc', 'DJytd4UhGgx', 'DJlOiPaBurB', 'DJd1irGBiNI', 'DJWG4_pBoXC', 'DJJbRoEBtUD', 'DJBtdm6hYOg', 'DI8lb5IBW0o', 'DI5zbAihUJ3', 'DI3Hl7XBx6x', 'DIsq9ZOBhPg', 'DIp8xiziQNt', 'DInuoC2hxdX', 'DIk-IcZhoMH', 'DIiOBLGheD2', 'DIfzSiehuVj', 'DIdXJasConI', 'DIaxsvDNBLc', 'DIYXRklBHL4', 'DIVx1V_NQ0H', 'DITgN0mh3LV']
 
 results = bulk_download_reels(shortcodes)
 
